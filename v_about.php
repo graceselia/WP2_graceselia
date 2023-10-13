9ec31a08d3b4fdebb3abe9b9a19c9214c282da96
graceselia <145754400+graceselia@users.noreply.github.com>
2023-10-13 16:17:09 +0700

    Add files via upload

---
+++ v_about.php
@@ -0,0 +1,24 @@
+<section> 
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4> 
+    <ul type="disc"> 
+        <li>Nama Depan : Grace</li> 
+        <li>Nama Belakang : selia</li> 
+    </ul> 
+    <br> 
+    <h4>Alamat</h4> 
+    <ul type="none"> 
+        <li> Krangkeng 41/15, Keden, Pedan, Klaten, Jawa tengah</li> 
+    </ul> 
+    
+    <h4>Tempat Lahir</h4> 
+    <ul type="none"> 
+        <li>Klaten</li> 
+    </ul> 
+    
+    <h4>Olah Raga Favorit</h4> 
+    <ul type="square"> 
+        <li>Bulutangkis</li> 
+        <li>Basket</li> 
+    </ul> 
+</section>
